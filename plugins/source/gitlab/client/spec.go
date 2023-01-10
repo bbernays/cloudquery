5bf8af89af6d97cdae274a7271a5fbf227ab9b7c
bbernays <ben@bernays.me>
2023-01-10 14:28:24 -0500

    Update spec.go

--- plugins/source/gitlab/client/spec.go
+++ plugins/source/gitlab/client/spec.go
@@ -1,6 +1,8 @@
 package client
 
-import "errors"
+import (
+	"errors"
+)
 
 type Spec struct {
 	Token   string `json:"access_token,omitempty"`
@@ -12,11 +14,6 @@ func (s Spec) Validate() error {
 	if gitlabToken == "" {
 		return errors.New("missing GitLab API token in configuration file")
 	}
-	if s.BaseURL == "" {
-		return errors.New("missing GitLab base URL in configuration file")
-	}
-	if s.BaseURL == "gitlab.com" {
-		return errors.New("base URL cannot be `gitlab.com` this plugin only supports self hosted instances")
-	}
+
 	return nil
 }
