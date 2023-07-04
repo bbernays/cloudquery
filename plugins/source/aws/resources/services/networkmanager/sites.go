c61075c41fc7737c80ed672f8ee0553c10c30d7b
bbernays <ben@bernays.me>
2023-07-04 16:20:12 -0400

    Create sites.go

---
+++ plugins/source/aws/resources/services/networkmanager/sites.go
@@ -0,0 +1,46 @@
+package networkmanager
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/networkmanager"
+	"github.com/aws/aws-sdk-go-v2/service/networkmanager/types"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v3/transformers"
+)
+
+func sites() *schema.Table {
+	tableName := "aws_networkmanager_sites"
+	return &schema.Table{
+		Name:        tableName,
+		Description: `https://docs.aws.amazon.com/networkmanager/latest/APIReference/API_Site.html`,
+		Resolver:    fetchSites,
+		Transform:   transformers.TransformWithStruct(&types.Site{}, transformers.WithPrimaryKeys("GlobalNetworkId", "SiteArn")),
+		Columns: []schema.Column{
+			client.DefaultAccountIDColumn(false),
+			client.DefaultRegionColumn(true),
+		},
+		Relations: schema.Tables{},
+	}
+}
+
+func fetchSites(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	cl := meta.(*client.Client)
+	svc := cl.Services().Networkmanager
+	globalNetwork := parent.Item.(types.GlobalNetwork)
+	input := &networkmanager.GetSitesInput{
+		GlobalNetworkId: globalNetwork.GlobalNetworkId,
+	}
+	paginator := networkmanager.NewGetSitesPaginator(svc, input)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx, func(options *networkmanager.Options) {
+			options.Region = cl.Region
+		})
+		if err != nil {
+			return err
+		}
+		res <- page.Sites
+	}
+	return nil
+}
